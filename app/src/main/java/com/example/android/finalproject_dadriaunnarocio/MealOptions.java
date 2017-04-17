48165e428a74d842bcb2bea5b1a918c5d3417837
rocionde <rec2146@tc.columbia.edu>
2017-04-17 18:48:48 -0400

    Student signUP layout

---
+++ app/src/main/java/com/example/android/finalproject_dadriaunnarocio/MealOptions.java
@@ -0,0 +1,13 @@
+package com.example.android.finalproject_dadriaunnarocio;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MealOptions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meal_options);
+    }
+}
